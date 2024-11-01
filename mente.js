e220ee6fb816a0e21e5b3a9e2f1831fe85df3226
MaxiMonardez <maximo.monardez@nogadev.com>
2024-10-31 21:28:45 -0300

    Al js, le añadi funciones, como el valor de los botones para que se muestre en pantalla y la suma de estos

--- mente.js
+++ mente.js
@@ -1,5 +1,36 @@
-var boton1 = 1;
+let pantalla = '';
+
+function valorBoton(valorDelNumero) {
+
+    
+    pantalla += valorDelNumero.toString(); 
+    document.getElementsByClassName("pantalla")[0].innerHTML = pantalla;
+    //.toString(), a lo que entiendo, devuelve el valor del objeto
+    //entonces, a la pantalla que no tiene valor inicial, se le suma
+    //el valor del numero y con el .toString, lo devuelve en la pantalla
+    
+}
+
+function resultadoBoton() {
+    try {
+        let resultado = eval(pantalla);
+        pantalla = resultado.toString();
+        document.getElementsByClassName("pantalla")[0].innerHTML = pantalla;
+    } catch (error)
+    {
+        document.getElementsByClassName("pantalla")[0].innerHTML = "Error";
+        pantalla = '';
+    }
+}
+//el try ejectuta ciertos bloques de instrucciones y si algo sale mal o no
+//hay algo previsto el catch se ejecuta y da mensaje de error, y vuelve 
+//la pantalla a no tener valor
+
+//el eval, como se insinua, evalua el valor de pantalla y lo ejecuta
 
 
 
-const botones = document.getElementsByClassName("botones");
+
+
+
+
